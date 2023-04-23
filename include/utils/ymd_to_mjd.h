00292bd90b5c58cd971cb927ad4fd240489a2cf7
michaelwro <michael.wrona77@gmail.com>
2023-04-23 17:53:55 +0000

    Removed whitespace

--- include/utils/ymd_to_mjd.h
+++ include/utils/ymd_to_mjd.h
@@ -13,14 +13,14 @@ namespace EopUtils {
 
 /**
  * @brief Convert Gregorian year/month/day date to mean Julian date (MJD).
- * 
+ *
  * @details Algorithm from "Astronomical Algorithms" 2nd ed. (Meeus).
- * 
+ *
  * @param year Year number.
  * @param month Month number [1-12].
  * @param day Day number [1-31].
  * @return Mean Julian date.
- * 
+ *
  * @exception std::domain_error Invalid date input.
  */
 unsigned int ymd_to_mjd(unsigned int year, unsigned int month, unsigned int day);
